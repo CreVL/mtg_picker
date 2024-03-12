87b76679fe93cc8ea1a7d8cc66060209fdde4ddf
CreVL <91658458+CreVL@users.noreply.github.com>
2024-03-12 23:51:38 +0300

    undo import

--- test/ui/controllers/card_controller/card_controller_test.dart
+++ test/ui/controllers/card_controller/card_controller_test.dart
@@ -6,7 +6,6 @@ import 'package:mtg_picker/application/repository/cards/cards_repository.dart';
 import 'package:mtg_picker/application/repository/favorite/favorite_card_repository.dart';
 import 'package:mtg_picker/application/repository/repository_error.dart';
 import 'package:mtg_picker/domain/entities/card/card.dart';
-import 'package:mtg_picker/domain/entities/favorite/favorite_card/favorite_card.dart';
 import 'package:mtg_picker/internal/entities/either.dart';
 import 'package:mtg_picker/ui/state_management/card_controller/card_controller.dart';
 
